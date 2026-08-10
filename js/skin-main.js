a7c6201b2420751d5c3f5db7bd04a234dc4a8385
goodbyestarwars <goodbyestarwars@gmail.com>
2026-08-10 23:22:51 +0900

    Bust realtime table script cache

--- js/skin-main.js
+++ js/skin-main.js
@@ -59,7 +59,7 @@
     var HOME_WIDGETS_SCRIPT_URL = document.currentScript && document.currentScript.src
       ? document.currentScript.src.replace(/skin-main(?:\.min)?\.js(?:\?.*)?$/, 'home-widgets.js?v=20260810-us-schedule')
       : 'https://goodbyestarwars.github.io/tistory-ticker/js/home-widgets.js?v=20260810-us-schedule';
-    var HOME_REALTIME_TABLE_SCRIPT_URL = 'https://goodbyestarwars.github.io/tistory-ticker/js/home-realtime-table.js?v=20260810-session12h-speed1';
+    var HOME_REALTIME_TABLE_SCRIPT_URL = 'https://goodbyestarwars.github.io/tistory-ticker/js/home-realtime-table.js?v=20260810-realtime-reconnect1';
     var HOME_ECONOMIC_NEWS_SCRIPT_URL = 'https://goodbyestarwars.github.io/tistory-ticker/js/home-economic-news.js?v=20260810-session12h-usnews-speed1';
 
     function escapeHomeHtml(value) {
