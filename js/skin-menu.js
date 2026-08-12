0fb31e38603d6995dd74f77b0e7bf211f665f101
goodbyestarwars <goodbyestarwars@gmail.com>
2026-08-12 23:11:54 +0900

    chore: 실시간 시세 메뉴명에 US 표기

--- js/skin-menu.js
+++ js/skin-menu.js
@@ -20,7 +20,7 @@
       label: '종목',
       children: [
         { href: '/page/foreign-flow', label: '종목분석' },
-        { href: '/page/stock-search', label: '실시간 시세' }
+        { href: '/page/stock-search', label: '실시간 시세 (US. Include)' }
       ]
     },
     {
