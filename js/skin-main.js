6b5467ba0f8905e8442717b7ced1dac73f135070
goodbyestarwars <goodbyestarwars@gmail.com>
2026-08-16 17:34:30 +0900

    Fix US company search in calendar

--- js/skin-main.js
+++ js/skin-main.js
@@ -93,7 +93,7 @@
     })();
 
     var GAS_TICKER_URL = 'https://script.google.com/macros/s/AKfycbzhKxOqOzw6N1xjW0Jhj5tlbiN0PMRdrQQD6nORBTlP0NDAOvtKfidHU2xwMAbV33mOuQ/exec';
-    var CALENDAR_SCRIPT_URL = 'https://goodbyestarwars.github.io/tistory-ticker/js/stock-calendar.js?v=20260815-company-name';
+    var CALENDAR_SCRIPT_URL = 'https://goodbyestarwars.github.io/tistory-ticker/js/stock-calendar.js?v=20260816-us-alias-search-v1';
     var HOME_WIDGETS_SCRIPT_URL = document.currentScript && document.currentScript.src
       ? document.currentScript.src.replace(/skin-main(?:\.min)?\.js(?:\?.*)?$/, 'home-widgets.js?v=20260816-market-switch-v3')
       : 'https://goodbyestarwars.github.io/tistory-ticker/js/home-widgets.js?v=20260816-market-switch-v3';
